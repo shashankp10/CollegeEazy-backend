cb7bff8f99c4b00054a618ccc5e4d41c170aa3ce
Shashank Pandey <102212264+shashankp10@users.noreply.github.com>
2023-02-20 21:07:16 +0530

    Update NotesRepo.java

--- src/main/java/com/project/repositories/NotesRepo.java
+++ src/main/java/com/project/repositories/NotesRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.entities.Notes;
 
 public interface NotesRepo extends JpaRepository<Notes, Long> {
-	List<Notes> findBySubjectIdAndType(String subjectId,String type	);
+	List<Notes> findBySubjectIdAndType(String subjectId,String type);
 
 }

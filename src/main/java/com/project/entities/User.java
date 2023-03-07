9a6d7b124685d13a074f8f6b6fec4f6b1b30de56
shashank pandey <pandey2001shashank@gmail.com>
2023-03-07 20:10:27 +0530

    updated APIs

--- src/main/java/com/project/entities/User.java
+++ src/main/java/com/project/entities/User.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
